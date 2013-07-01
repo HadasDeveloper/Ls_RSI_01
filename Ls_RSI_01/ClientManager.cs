ed830c8d3832555ab48417978ea011a9d5c0449a
Kapelushnik ALLA <Allakape@gmail.com>
2013-07-01 17:25:31 +0300

    4

--- Ls_RSI_01/ClientManager.cs
+++ Ls_RSI_01/ClientManager.cs
@@ -169,13 +169,13 @@ namespace Ls_RSI_01
                 Thread.Sleep(15000); // 15 seconds
 
                 KeySender key = new KeySender();
-                for (int i = 0; i < 10; i++)
-                {
-                    key.Send(runTwsProcesId);
-                    Logger.WriteToLog(DateTime.Now, string.Format("After sending kay.send at {0} time",i), Program.UserId);
-                    Thread.Sleep(6000); // 6 seconds
+                //for (int i = 0; i < 10; i++)
+                //{
+                key.Send(runTwsProcesId);
+                Logger.WriteToLog(DateTime.Now, string.Format("After sending kay.send"), Program.UserId);
+                Thread.Sleep(60000); // 60 seconds
 
-                }
+                //}
                  //Thread.Sleep(60000); // 60 seconds
                 
             }  
